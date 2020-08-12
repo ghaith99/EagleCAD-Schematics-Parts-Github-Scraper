bf2a4e5e5dbf6bacbf4d80c50b1e002883bd84a6
Ghaith AlGhaith <gh4ith@gmail.com>
2020-08-12 20:06:58 +0300

    Parsing EagleCAD parts from sch files of github organization

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests 
+import xml.etree.ElementTree as ET
+import json
+import re
+import csv
+import sys
+import sys, getopt
+
+
+def main(argv):
+    pages = 2
+    per_page = 5
+    org = "sparkfun"
+
+    try:
+        opts, args = getopt.getopt(argv,"hp:c:o:",["page=","count_per_page=", "organization="])
+    except getopt.GetoptError:
+        print ('main.py -o <organization> -c <count_per_page> -p <page>')
+        sys.exit(2)
+    for opt, arg in opts:
+        print(opt)
+        print(arg)
+        if opt == '-h':
+            print ('main.py -pp <per_page> -p <p>')
+            sys.exit()
+        elif opt in ("-p", "--page"):
+            pages = arg
+        elif opt in ("-c", "--count_per_page"):
+            per_page = arg
+        elif opt in ("-o", "--organization"):
+            org = arg
+
+    for page in range(1,int(pages)+1):
+        print("Parsing page #"+str(page))
+        url = "https://api.github.com/search/code?q=org:"+org+"+extension:sch&per_page="+str(per_page)+"&sort=indexed&order=dsc&page="+ str(page)
+        data = json.loads(requests.get(url).text)
+        with open('parts.csv', 'w', newline='', encoding="utf-8") as csvfile:
+            fieldnames = ['library', 'deviceset', 'device', 'value']
+            parts_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            parts_writer.writeheader()
+            for e in data['items']:
+                link = e["html_url"].replace("https://github.com", "https://raw.githubusercontent.com").replace("/blob","")
+                print("Parsing ==>:"+link)
+                root = ET.fromstring(requests.get(link).text) 
+                for p in root.find('drawing').find('schematic').find('parts'):
+                    parts_writer.writerow({'library':p.get('library'), 'deviceset':p.get('deviceset'), 'device':p.get('device'), 'value': p.get('value')})
+                
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
